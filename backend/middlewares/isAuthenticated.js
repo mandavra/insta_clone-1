a8a741123342ac79ab47be1fc4c7d1af3933a40d
Man 150 <man.davra@swiftrut.com>
2025-01-08 03:31:55 +0530

    Add debug log to output all cookies in isAuthenticated middleware

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req,res,next)=>{
     try {
         const token = req.cookies.token;
+        console.log(req.cookies);  // Output all cookies
         console.log("🚀 ~ isAuthenticated ~ token:", token)
         if(!token){
             return res.status(401).json({
