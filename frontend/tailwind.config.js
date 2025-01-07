5318c1c6a4ad75f487356ccbe93fce4d5a777031
Man 150 <man.davra@swiftrut.com>
2025-01-07 06:46:12 +0530

    Update Tailwind CSS configuration to use ES module syntax and include additional content paths

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
     './src/**/*.{html,js,jsx}',
   ],
   prefix: "",
@@ -31,4 +34,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
