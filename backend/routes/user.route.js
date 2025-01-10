cc3540de8f626a1d0121607e111221150cc8fe1b
Man 150 <man.davra@swiftrut.com>
2025-01-10 07:19:02 +0530

    Fix import path casing for User.controller.js in user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { editProfile, follow, followOrUnfollow, getProfile, login, logout, register, unfollow } from "../controllers/User.controller.js";
+import { editProfile, follow, followOrUnfollow, getProfile, login, logout, register, unfollow } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../middlewares/multer.js";
 
